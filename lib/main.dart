11367688673312e820760c1ede25a357e3ef8efd
LeNguyenPhiNhungB1910426 <nhungb1910426@student.ctu.edu.vn>
2023-03-26 21:16:20 +0700

    Luu san pham yeu thich len Realtime Database cua Firebase

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'ui/screens.dart';
+
+Future<void> main() async {
+  //(1)Load the .env file
+  await dotenv.load();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        //(2)Create and provider AuthManager
+        ChangeNotifierProvider(
+          create: (ctx) => AuthManager(),
+        ),
+        ChangeNotifierProxyProvider<AuthManager, ProductsManager>(
+          create: (ctx) => ProductsManager(),
+          update: (ctx, authManager, productsManager) {
+            productsManager!.authToken = authManager.authToken;
+            return productsManager;
+          },
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => CartManager(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => OrdersManager(),
+        ),
+      ],
+      //(3)Consume the AuthManager instance
+      child: Consumer<AuthManager>(
+        builder: (ctx, authManager, child) {
+          return MaterialApp(
+            title: 'My Shop',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+                fontFamily: 'Lato',
+                colorScheme: ColorScheme.fromSwatch(
+                  primarySwatch: Colors.blue,
+                ).copyWith(
+                  secondary: Colors.deepOrange,
+                )),
+            // home: const SafeArea(
+            //   // child: ProductDetailScreen(
+            //   //   ProductsManager().items[0],
+            //   //),
+            //   //   child: ProductsOverviewScreen(),
+            //   // ),
+            //   // child: UserProductScreen(),
+            //   // child: CartScreen(),
+            //   child: OrdersScreen(),
+            // ),
+            // home: const ProductsOverviewScreen(),
+            home: authManager.isAuth
+                ? const ProductsOverviewScreen()
+                : FutureBuilder(
+                    future: authManager.tryAutoLogin(),
+                    builder: (context, snapshot) {
+                      return snapshot.connectionState == ConnectionState.waiting
+                          ? const SplashScreen()
+                          : const AuthScreen();
+                    },
+                  ),
+            routes: {
+              CartScreen.routeName: (ctx) => const CartScreen(),
+              OrdersScreen.routeName: (ctx) => const OrdersScreen(),
+              UserProductScreen.routeName: (ctx) => const UserProductScreen(),
+            },
+            onGenerateRoute: (settings) {
+              if (settings.name == ProductDetailScreen.routeName) {
+                final productId = settings.arguments as String;
+                return MaterialPageRoute(
+                  builder: (ctx) {
+                    return ProductDetailScreen(
+                      ctx.read<ProductsManager>().findById(productId),
+                    );
+                  },
+                );
+              }
+              if (settings.name == EditProductScreen.routeName) {
+                final productId = settings.arguments as String?;
+                return MaterialPageRoute(
+                  builder: (ctx) {
+                    return EditProductScreen(
+                      productId != null
+                          ? ctx.read<ProductsManager>().findById(productId)
+                          : null,
+                    );
+                  },
+                );
+              }
+              return null;
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/ui/products/user_products_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'user_product_list_tile.dart';
+import 'products_manager.dart';
+import '../shared/app_drawer.dart';
+import 'edit_product_screen.dart';
+
+class UserProductScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+  const UserProductScreen({super.key});
+
+  Future<void> _refreshProducts(BuildContext context) async {
+    await context.read<ProductsManager>().fetchProducts(true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final productsManager = ProductsManager();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: <Widget>[
+          buildAddButton(context),
+        ],
+      ),
+      drawer: const AppDrawer(),
+      body: FutureBuilder(
+        future: _refreshProducts(context),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return RefreshIndicator(
+            child: buildUserProductListView(productsManager),
+            onRefresh: () => _refreshProducts(context),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildUserProductListView(ProductsManager productsManager) {
+    return Consumer<ProductsManager>(
+      builder: (ctx, productsManager, child) {
+        return ListView.builder(
+          itemCount: productsManager.itemCount,
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              UserProductListTile(productsManager.items[i]),
+              const Divider(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget buildAddButton(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        Navigator.of(context).pushNamed(EditProductScreen.routeName);
+      },
+      icon: const Icon(Icons.add),
+    );
+  }
+}
